05ff6f015a31806d297c1e8207bce031db32da7e
Alexandra <a.chigrina89@gmail.com>
2025-02-06 17:04:51 +0300

    add tests for fnc

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Category:
     name: str
     description: str
@@ -5,7 +8,7 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name: str, description: str, products: Optional[list] = None) -> None:
         self.name = name
         self.description = description
         self.products = products if products else []
--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,44 @@
 import json
-import os
+from pathlib import Path
+
 from config import PATH_TO_PRODUCTS_DATA
-
-from src.product import Product
 from src.category import Category
+from src.product import Product
 
 
-def read_json(path: str) -> dict:
+def read_json(path: str | Path) -> list[dict]:
     """Принимает на вход путь до JSON-файла, возвращает словарь с данными"""
 
-    full_path =os.path.abspath(path)
-    with open(full_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            if isinstance(data, list):
+                return data
+            else:
+                return []
+    except json.JSONDecodeError:
+        return []
+
+    except FileNotFoundError:
+        return []
 
 
-def create_objects_from_json(data):
+def create_objects_from_json(data: list[dict]) -> list[Category]:
     """Создает объекты классов"""
     categories = []
     for category in data:
         products = []
-        for product in category['products']:
-            products.append(Product(**product))   # распаковываем словарь
-        category['products'] = products
+        for product in category["products"]:
+            products.append(Product(**product))  # распаковываем словарь
+        category["products"] = products
         categories.append(Category(**category))
 
     return categories
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     original_data = read_json(PATH_TO_PRODUCTS_DATA)
+    print(original_data)
     categories_data = create_objects_from_json(original_data)
-    print(categories_data[0].name)
+    print(categories_data[1].name)
     print(categories_data[0].products)
