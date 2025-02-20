793d08af07fb3e038e53d4eb605270e371e927ce
Alexandra <a.chigrina89@gmail.com>
2025-02-20 16:37:09 +0300

    add smartphone & lawngrass classes

--- src/product.py
+++ src/product.py
@@ -13,7 +13,9 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other: "Product") -> float:
-        return self.price * self.quantity + other.price * other.quantity
+        if type(other) is Product:
+            return self.price * self.quantity + other.price * other.quantity
+        raise TypeError
 
     @property
     def price(self) -> float:
