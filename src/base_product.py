d52eed98b2b48ba5a50afcd6e7cce546b95eb749
Alexandra <a.chigrina89@gmail.com>
2025-02-25 14:57:26 +0300

    add base_product class & mixin

---
+++ src/base_product.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+    """ Абстрактный базовый класс для всех продуктов """
+
+    @classmethod
+    @abstractmethod
+    def new_product(cls, *args, **kwargs):
+        pass
